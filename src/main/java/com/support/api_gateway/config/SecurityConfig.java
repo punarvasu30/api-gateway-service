53c6121674b9717a0de893ad329fa2bf05387f65
Punarvasu20003 <punarvasuingole72@gmail.com>
2026-01-15 20:50:17 +0530

    security config

--- src/main/java/com/support/api_gateway/config/SecurityConfig.java
+++ src/main/java/com/support/api_gateway/config/SecurityConfig.java
@@ -1,16 +1,17 @@
 package com.support.api_gateway.config;
 
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.oauth2.jwt.NimbusReactiveJwtDecoder;
 import org.springframework.security.oauth2.jwt.ReactiveJwtDecoder;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-
-import javax.crypto.SecretKey;
-import javax.crypto.spec.SecretKeySpec;
 
 @Configuration
 @EnableWebFluxSecurity
@@ -25,6 +26,7 @@ public class SecurityConfig {
         return http
                 .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers(HttpMethod.OPTIONS,"/**").permitAll()
                         .pathMatchers("/auth/**").permitAll()
                         .pathMatchers("/support/**").authenticated()
                         .anyExchange().denyAll()
